3a06238ac595018de4d7b37c9a25638380f3a8fd
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-20 01:48:22 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -30,7 +30,28 @@
 #   sub_category: nil
 # }
 def naming_system
-hash= {}
+giant_hash = {
+  :label=>"Kingdom",
+  :sub_category=>{
+    :label=>"Phylum",
+    :sub_category=> {
+      :label=>"Class",
+      :sub_category=> {
+        :label=>"Order",
+        :sub_category=> {
+          :label=>"Family",
+          :sub_category=> {
+            :label=>"Genus",
+            :sub_category=> {
+              :label=>"Species",
+              :sub_category=>nil
+              }
+            }
+          }
+        }
+      }
+    }
+  }
   #  Kingdom
   #  Phylum
   #  Class
